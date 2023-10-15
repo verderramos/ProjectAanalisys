c8ca3d7ceac2d40fc0cb891f9572e38ff3bc108d
Andre Luiz Verderramos da Silva <92328516+verderramos@users.noreply.github.com>
2023-10-14 23:52:07 -0300

    Ac 2 - 14.10.2023
    Acesso aos dados, alteração de dados e exclusão de clientes.

---
+++ ClienteBLL.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Loja.Models;
+using Loja.DAL;
+
+namespace Loja.BLL
+{
+    public class ClienteBLL : IClienteDados
+    {
+        private ClienteDAL dal;
+
+        public ClienteBLL()
+        {
+            this.dal = new ClienteDAL();
+        }
+        public void Alterar(Cliente cliente)
+        {
+            Validar(cliente);
+            if (string.IsNullOrEmpty(cliente.Id))
+            {
+                throw new Exception("O Id deve ser informado.");
+            }
+            dal.Alterar(cliente);
+        }
+
+        public void Excluir(string Id)
+        {
+            if (string.IsNullOrEmpty(Id))
+            {
+                throw new Exception("O Id deve ser informado.");
+            }
+            dal.Excluir(Id);
+        }
+
+        public void Incluir(Cliente cliente)
+        {
+            Validar(cliente);
+            if (string.IsNullOrEmpty(cliente.Id))
+            {
+                cliente.Id = Guid.NewGuid().ToString();
+            }
+            dal.Incluir(cliente);
+        }
+
+        private static void Validar(Cliente cliente)
+        {
+            if (string.IsNullOrEmpty(cliente.Nome))
+            {
+                throw new ApplicationException("O nome deve ser informado");
+            }
+        }
+
+        public Cliente ObterPorEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente ObterPorId(string id)
+        {
+            return dal.ObterPorId(id);
+        }
+
+        public List<Cliente> ObterTodos()
+        {
+            var lista = dal.ObterTodos();
+            return lista;
+        }
+    }
+}
---
+++ ClienteDAL.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Loja.Models;
+
+namespace Loja.DAL
+{
+    public class ClienteDAL : IClienteDados
+    {
+        public void Alterar(Cliente cliente)
+        {
+
+            DbHelper.ExecuteNonQuery("ClienteAlterar",
+                "@Id", cliente.Id,
+                "@Nome", cliente.Nome,
+                "@Email", cliente.Email,
+                "@Telefone", cliente.Telefone
+                );
+        }
+
+        public void Excluir(string Id)
+        {
+            Cliente cliente = ObterPorId(Id);
+            SerializadorHelper.Serializar(@"C: \Users\verde\OneDrive\Área de Trabalho\teste.xml", cliente);
+            DbHelper.ExecuteNonQuery("ClienteExcluir", "@Id", Id);
+        }
+
+        public void Incluir(Cliente cliente)
+        {
+            DbHelper.ExecuteNonQuery("ClienteIncluir",
+                "@Id",cliente.Id,
+                "@Nome",cliente.Nome,
+                "@Email",cliente.Email,
+                "@Telefone",cliente.Telefone
+                );
+        }
+
+        public Cliente ObterPorEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente ObterPorId(string id)
+        {
+            Cliente cliente = null;
+            using (var reader = DbHelper.ExecuteReader("ClienteObterPorId", "@Id", id))
+            {
+                if (reader.Read())
+                {
+                    cliente = ObterClienteReader(reader);
+                }
+            }
+            return cliente;
+        }
+
+        public List<Cliente> ObterTodos()
+        {
+            var lista = new List<Cliente>();
+            using (var reader = DbHelper.ExecuteReader("ClienteListar"))
+            {
+                while (reader.Read())
+                {
+                    Cliente cliente = ObterClienteReader(reader);
+                    lista.Add(cliente);
+                }
+            }
+            return lista;
+        }
+
+        private static Cliente ObterClienteReader(System.Data.IDataReader reader)
+        {
+            var cliente = new Cliente();
+            cliente.Id = reader["Id"].ToString();
+            cliente.Nome = reader["Nome"].ToString();
+            cliente.Email = reader["Email"].ToString();
+            cliente.Telefone = reader["Telefone"].ToString();
+            return cliente;
+        }
+    }
+}
