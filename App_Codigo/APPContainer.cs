584e72135c790e23f42d036031036ba3e8160f6e
Andre Luiz Verderramos da Silva <92328516+verderramos@users.noreply.github.com>
2023-11-03 15:20:34 -0300

    Entrega AC3 - LojaUIWEB p1
    Pasta Loja.UI.Web, pastas App_codigo, App_data, App_Start e bin.

---
+++ App_Codigo/APPContainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Loja.BLL;
+using Loja.DAL;
+using Loja.Models;
+
+namespace Loja.UI.Web
+{
+    public class APPContainer
+    {
+        public static IClienteDados ObterClienteBLL()
+        {
+            var dal = new ClienteDAL();
+            var bll = new ClienteBLL(dal);
+            return bll;
+        }
+        public static IProdutosDados ObterProdutosbll()
+        {
+            var dal = new ProdutoDAL();
+            var bll = new ProdutoBLL(dal);
+            return bll;
+        }
+    }
+}
